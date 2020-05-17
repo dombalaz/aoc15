3bb2c55a4d7d9f9f23af086c5f95cf3ad4930f5e
Dominik Baláž <dombalaz@protonmail.com>
2020-05-17 22:22:07 +0200

    Add solution to 3_2

--- src/aoc15_d03.cpp
+++ src/aoc15_d03.cpp
@@ -10,6 +10,8 @@ void printHelp();
 
 void solveTaskOne(const std::string &l);
 
+void solveTaskTwo(const std::string &l);
+
 int main(int argc, char **argv)
 {
     if(argc != 2) {
@@ -26,6 +28,7 @@ int main(int argc, char **argv)
     f.close();
 
     solveTaskOne(l);
+    solveTaskTwo(l);
 
     return EXIT_SUCCESS;
 }
@@ -45,4 +48,11 @@ void solveTaskOne(const std::string &l)
     auto r = visitedHousesBySanta(l);
     std::cout << binName << ": ";
     std::cout << "Solution to day three task one is " << r << std::endl;
+}
+
+void solveTaskTwo(const std::string &l)
+{
+    auto r = visitedHousesBySantaAndRobot(l);
+    std::cout << binName << ": ";
+    std::cout << "Solution to day three task two is " << r << std::endl;
 }
