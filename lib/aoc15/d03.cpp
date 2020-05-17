a50bd093b5f1fd44d9701ded503d6c75e2f82556
Dominik Baláž <dombalaz@protonmail.com>
2020-05-17 21:49:03 +0200

    Implement library func for task one

--- lib/aoc15/d03.cpp
+++ lib/aoc15/d03.cpp
@@ -1,8 +1,29 @@
 #include <aoc15/d03.h>
 
+#include <utility>
+#include <set>
+
 uint visitedHousesBySanta(const std::string &in)
 {
-    // TODO implement.
-    (void)in;
-    return 0;
+    std::set<std::pair<int, int>> grid{{0, 0}};
+    int x{0};
+    int y{0};
+    for (const auto &c : in) {
+        switch (c) {
+        case '^':
+            ++y;
+            break;
+        case '>':
+            ++x;
+            break;
+        case 'v':
+            --y;
+            break;
+        case '<':
+            --x;
+            break;
+        }
+        grid.insert({x, y});
+    }
+    return grid.size();
 }
