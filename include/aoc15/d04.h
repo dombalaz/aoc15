920e6505e1aa3311d1373bfd1401aba59c8ebdcd
Dominik Baláž <dombalaz@gmail.com>
2020-05-20 08:41:34 +0200

    Add 4_1 interface and tests

---
+++ include/aoc15/d04.h
@@ -0,0 +1,8 @@
+#ifndef AOC15_D04_H
+#define AOC15_D04_H
+
+#include <string>
+
+int miningNumber(const std::string &key, int leadingZeroes);
+
+#endif // AOC15_D04_H
---
+++ lib/aoc15/d04.cpp
@@ -0,0 +1,8 @@
+#include <aoc15/d04.h>
+
+int miningNumber(const std::string &key, int leadingZeroes)
+{
+    (void)key;
+    (void)leadingZeroes;
+    return -1;
+}
