977d140824fc39be8ad18ffc3b63b71cc410ee8a
Dominik Baláž <dominik.balaz@mavenir.com>
2020-09-09 14:51:07 +0200

    Add cppchek to runner and fix all issues

--- lib/aoc15/d01.cpp
+++ lib/aoc15/d01.cpp
@@ -1,12 +1,10 @@
 #include <aoc15/d01.h>
 
+#include <numeric>
+
 auto getFloor(const std::string &in) -> int
 {
-    int r{0};
-    for (const auto &c : in) {
-        r += c == '(' ? 1 : -1;
-    }
-    return r;
+    return std::accumulate(in.begin(), in.end(), 0, [](const auto &a, const auto &b) { return a + (b == '(' ? 1 : -1); });
 }
 
 auto getIndexOfFirstBasement(const std::string &in) -> int
--- lib/aoc15/d09.cpp
+++ lib/aoc15/d09.cpp
@@ -14,30 +14,25 @@ auto shortestDistance(const std::vector<std::string> &v) -> size_t
     std::smatch match;
     for (const auto &s : v) {
         std::regex_match(s, match, regex);
-        if (set.count(match[1]) == 0) {
-            set.insert(match[1]);
-        }
-        if (set.count(match[2]) == 0) {
-            set.insert(match[2]);
-        }
+        set.insert(match[1]);
+        set.insert(match[2]);
         distances.insert({{match[1], match[2]}, std::stoul(match[3])});
         distances.insert({{match[2], match[1]}, std::stoul(match[3])});
     }
     std::vector<std::string> perms(set.begin(), set.end());
-    auto orig = perms;
-    auto shortestDistance = std::numeric_limits<size_t>::max();
+    auto res = std::numeric_limits<size_t>::max();
     std::sort(perms.begin(), perms.end());
     do {
         auto r{0U};
         for (auto i = 0U; i < perms.size() - 1; ++i) {
             r += distances[{perms.at(i), perms.at(i + 1)}];
         }
-        if (r < shortestDistance) {
-            shortestDistance = r;
+        if (r < res) {
+            res = r;
         }
     } while (std::next_permutation(perms.begin(), perms.end()));
 
-    return shortestDistance;
+    return res;
 }
 
 auto longestDistance(const std::vector<std::string> &v) -> size_t
@@ -46,28 +41,23 @@ auto longestDistance(const std::vector<std::string> &v) -> size_t
     std::smatch match;
     for (const auto &s : v) {
         std::regex_match(s, match, regex);
-        if (set.count(match[1]) == 0) {
-            set.insert(match[1]);
-        }
-        if (set.count(match[2]) == 0) {
-            set.insert(match[2]);
-        }
+        set.insert(match[1]);
+        set.insert(match[2]);
         distances.insert({{match[1], match[2]}, std::stoul(match[3])});
         distances.insert({{match[2], match[1]}, std::stoul(match[3])});
     }
     std::vector<std::string> perms(set.begin(), set.end());
-    auto orig = perms;
-    auto shortestDistance = 0U;
+    auto res = 0U;
     std::sort(perms.begin(), perms.end());
     do {
         auto r{0U};
         for (auto i = 0U; i < perms.size() - 1; ++i) {
             r += distances[{perms.at(i), perms.at(i + 1)}];
         }
-        if (r > shortestDistance) {
-            shortestDistance = r;
+        if (r > res) {
+            res = r;
         }
     } while (std::next_permutation(perms.begin(), perms.end()));
 
-    return shortestDistance;
+    return res;
 }
